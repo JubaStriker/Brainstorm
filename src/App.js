533568920ff9dd237a70a9136bcf73a8d50e0125
JubaStriker <jubastriker@gmail.com>
2022-10-10 22:28:34 +0600

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import Nav from './components/NavBar';
+
 
 function App() {
   return (
     <div className="App">
+
+      <Nav />
+
+
 
     </div>
   );
